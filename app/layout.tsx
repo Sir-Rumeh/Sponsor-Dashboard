841d3b785b74b284bba70d043644279708894aae
Nosa Imuentinyan <156332108+nosa-imuentinyan-react-frontend-fcmb@users.noreply.github.com>
2025-08-12 13:38:50 +0100

    initial dashboard setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,51 +3,48 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { LoadingProvider } from "@/contexts/LoadingContext";
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
+	subsets: ["latin"],
+	display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Wowdash - Tailwind & Next.js Admin Dashboard with shadcn UI (Typescript)",
-  description: "WowDash - Admin Dashboard Multipurpose Next.js, TypeScript, ShadCn UI & Tailwind Template",
-  metadataBase: new URL("https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app"),
-  openGraph: {
-    title: "Wowdash - Admin Dashboard UI",
-    description: "A modern, responsive admin dashboard template built with Next.js, Tailwind CSS, and ShadCN UI.",
-    url: "https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app",
-    siteName: "Wowdash",
-    images: [
-      {
-        url: "https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Wowdash Admin Dashboard Preview",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Wowdash - Admin Dashboard UI",
-    description: "A modern, responsive admin dashboard template built with Next.js, Tailwind CSS, and ShadCN UI.",
-    images: ["https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app/og-image.jpg"],
-  },
+	title: "Survey Plus Sponsors Dashboard (Next.js & Typescript)",
+	description: "WowDash - Admin Dashboard Multipurpose Next.js, TypeScript, ShadCn UI & Tailwind Template",
+	metadataBase: new URL("https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app"),
+	openGraph: {
+		title: "Wowdash - Admin Dashboard UI",
+		description: "A modern, responsive admin dashboard template built with Next.js, Tailwind CSS, and ShadCN UI.",
+		url: "https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app",
+		siteName: "Wowdash",
+		images: [
+			{
+				url: "https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app/og-image.jpg",
+				width: 1200,
+				height: 630,
+				alt: "Wowdash Admin Dashboard Preview",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Wowdash - Admin Dashboard UI",
+		description: "A modern, responsive admin dashboard template built with Next.js, Tailwind CSS, and ShadCN UI.",
+		images: ["https://wowdash-nextjs-typescript-shadcn-5fu5.vercel.app/og-image.jpg"],
+	},
 };
 
-
 export default async function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
-        <LoadingProvider>
-          {children}
-        </LoadingProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${inter.className} antialiased`}>
+				<LoadingProvider>{children}</LoadingProvider>
+			</body>
+		</html>
+	);
 }
